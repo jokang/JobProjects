d001cd3b50b8e9f3178e4eebcaa5c5b0ed6520c7
zhoukang <zhoukang123>
2018-08-09 01:23:23 +0800

    Fix

--- algorithms/src/main/java/cn/jokang/algorithms/IsPopOrderOfStack.java
+++ algorithms/src/main/java/cn/jokang/algorithms/IsPopOrderOfStack.java
@@ -4,29 +4,37 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 判断是否是栈的弹出序列。 《剑指Offer》22
+ * 判断是否是栈的弹出序列。
+ * 《剑指Offer》题22
+ * 1.合法的出栈序列是卡塔兰数；
+ * 2.为什么规定没有相等元素？
  */
 public class IsPopOrderOfStack {
 
   public boolean isPopOrderOfStack(List<Integer> stackData, List<Integer> popOrder) {
-    boolean isPossible = false;
-
     if (stackData == null || popOrder == null || stackData.size() == 0 || popOrder.size() == 0
-        || stackData.size() != popOrder.size()) {
+        || stackData.size() < popOrder.size()) {
       return false;
     }
 
     Stack<Integer> tmpStack = new Stack<>();
     int stackDataIdx = 0;
     int checkingPopOrderIdx = 0;
-    while (stackDataIdx < stackData.size()) {
-      if (tmpStack.size() > 0 && tmpStack.peek().equals(popOrder.get(checkingPopOrderIdx))) {
-        tmpStack.pop();
-        checkingPopOrderIdx += 1;
-      } else {
+    while (checkingPopOrderIdx < popOrder.size()) {
+      while (tmpStack.isEmpty() || !tmpStack.peek().equals(popOrder.get(checkingPopOrderIdx))) {
+        if (stackDataIdx >= stackData.size()) {
+          break; // 无元素可压栈
+        }
         tmpStack.push(stackData.get(stackDataIdx));
-        stackDataIdx += 1;
+        stackDataIdx++;
       }
+
+      if (!tmpStack.peek().equals(popOrder.get(checkingPopOrderIdx))) {
+        break;
+      }
+
+      tmpStack.pop();
+      checkingPopOrderIdx += 1;
     }
 
     return checkingPopOrderIdx == popOrder.size();
--- algorithms/src/test/java/cn/jokang/algorithms/test/IsPopOrderOfStackTest.java
+++ algorithms/src/test/java/cn/jokang/algorithms/test/IsPopOrderOfStackTest.java
@@ -2,6 +2,7 @@ package cn.jokang.algorithms.test;
 
 import cn.jokang.algorithms.IsPopOrderOfStack;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,23 +12,17 @@ public class IsPopOrderOfStackTest {
   @Test
   public void test1() {
     IsPopOrderOfStack isPopOrderOfStack = new IsPopOrderOfStack();
-    List<Integer> stackData = new ArrayList<Integer>() {{
-      add(1);
-      add(2);
-      add(3);
-      add(4);
-      add(5);
-    }};
-
-    List<Integer> popOrder = new ArrayList<Integer>() {{
-      add(4);
-      add(5);
-      add(3);
-      add(2);
-      add(1);
-    }};
-
+    List<Integer> stackData = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    List<Integer> popOrder = new ArrayList<>(Arrays.asList(4, 5, 3, 2, 1));
     Assert.assertTrue(isPopOrderOfStack.isPopOrderOfStack(stackData, popOrder));
   }
 
+  @Test
+  public void test2() {
+    IsPopOrderOfStack isPopOrderOfStack = new IsPopOrderOfStack();
+    List<Integer> stackData = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    List<Integer> popOrder = new ArrayList<>(Arrays.asList(4, 3, 5, 1, 2));
+
+    Assert.assertFalse(isPopOrderOfStack.isPopOrderOfStack(stackData, popOrder));
+  }
 }

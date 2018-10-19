73f0747933ce176888b57d088fd2f38ca86c9ff1
zhoukang <zhoukang123>
2018-10-19 17:25:57 +0800

    Combinations

--- algorithms/src/main/java/cn/jokang/algorithms/leetcode/Combinations.java
+++ algorithms/src/main/java/cn/jokang/algorithms/leetcode/Combinations.java
@@ -1,45 +1,33 @@
 package cn.jokang.algorithms.leetcode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @SuppressWarnings({"Duplicates", "WeakerAccess", "SameParameterValue"})
 public class Combinations {
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int[] current = new int[k];
-        Set<Integer> s = new HashSet<>(n);
-        for (int i = 0; i < n; i++) {
-            s.add(i + 1);
-        }
-        backtrack(ans, 0, current, s);
 
-        return ans;
-    }
+  public List<List<Integer>> combine(int n, int k) {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> solution = new ArrayList<>();
+    backtrack(ans, solution, 1, n, k);
 
-    private void backtrack(List<List<Integer>> ans, int depth, int[] current, Set<Integer> candidates) {
-        if (current.length == depth) {
-            List<Integer> solution = new ArrayList<>();
-            for (int i : current) {
-                solution.add(i);
-            }
-            ans.add(solution);
-            return;
-        }
-        Set<Integer> cs = new HashSet<>(candidates);
-        for (int n : candidates) {
-            current[depth] = n;
-            if (depth >= 1 && current[depth - 1] > n) continue;
-            cs.remove(n);
-            backtrack(ans, depth + 1, current, cs);
-            cs.add(n);
-        }
-    }
+    return ans;
+  }
 
-    public static void main(String[] args) {
-        Combinations c = new Combinations();
-        System.out.println(c.combine(4, 2));
+  private void backtrack(List<List<Integer>> ans, List<Integer> solution, int start, int n, int remain) {
+    if (remain == 0) {
+      ans.add(new ArrayList<>(solution));
+      return;
     }
+    for (int i = start; i <= n; i++) {
+      solution.add(i);
+      backtrack(ans, solution, i + 1, n, remain - 1);
+      solution.remove(solution.size() - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    Combinations c = new Combinations();
+    System.out.println(c.combine(4, 2));
+  }
 }
